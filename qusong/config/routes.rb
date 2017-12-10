983cd8b686f48affd6764b3ebc3fa2bcf832d367
Sullivan <17126164@bjtu.edu.cn>
2017-12-10 21:26:33 +0800

    12.10

---
+++ qusong/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :couriers
+
+  resources :waybills do 
+    member do 
+      put :accept
+      put :reject
+      put :yiqu
+    end
+  end
+
+  get 'home', to: 'home#index'
+
+  root to: 'home#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ qusong/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+DatabaseCleaner.strategy = :truncation, {:only => [:couriers, :couriers_stations]}
+DatabaseCleaner.clean
+
+city = City.first
+
+city.stations.each do |station|
+  1.upto(5).each do |i|
+    courier = Courier.create(email: "courier#{i}_station_#{station.id}@washing.com", password: '111111', city: city)
+    station.couriers << courier
+  end
+end
+
